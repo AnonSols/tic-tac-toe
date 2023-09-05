f2bf679a5ac761916445cd3fc7e6f657db903713
Egede Solomon <soloschmail@gmail.com>
2023-09-05 17:08:31 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+// const val = document.getElementsByTagName('div');
+// const resetBtn = document.getElementById('btn');
+// const one = document.getElementById('one')
+// const two = document.getElementById('two')
+// const three = document.getElementById('three')
+// const four = document.getElementById('four')
+// const five = document.getElementById('five')
+// const six = document.getElementById('six')
+// const seven = document.getElementById('seven')
+// const eight = document.getElementById('eight')
+// const nine = document.getElementById('nine')
+
+// function clickHandler(){
+//     for (const item of val) {
+//        item.onclick = ()=>{
+//         if (item.getAttribute('aria-checked') == 'false') {
+//             item.innerHTML = `<span> X </span>`
+//             item.setAttribute('aria-checked', 'true')
+//         }else alert('Play your turn! ')
+    
+//        }
+//     }
+    
+
+// }
+
+// function resetHandler() {
+//     resetBtn.onclick = () => {
+//         for (const item of val) {
+//             item.setAttribute('aria-checked', false)
+//             item.innerText = ""
+//         }
+//     }
+// }
+
+// // function checkWins() { 
+// //     if ((one.innerText == five.innerText == nine.innerText) !== '')
+// // }
+
+// resetHandler()
+// clickHandler()
+
